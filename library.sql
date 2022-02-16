e1167a21e4a51fac975be3fabca00b14fda95321
SamRus34 <98973261+SamRus34@users.noreply.github.com>
2022-02-16 17:14:24 +0300

    бд, postman коллекция

---
+++ library.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 16 2022 г., 17:12
+-- Версия сервера: 8.0.24
+-- Версия PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `book`
+--
+
+CREATE TABLE `book` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `book`
+--
+
+INSERT INTO `book` (`id`, `name`, `genre`, `author`) VALUES
+(1, 'PHP для чайников', 'Научно-популярная', 'Билл Гейтс'),
+(2, 'История России', 'Историческая', 'Стивен Сигал'),
+(3, 'В поисках золотого дождя', 'Приключения', 'Индиана Джонс'),
+(4, 'Маслята в лесу', 'Ужасы', 'Грибник Абдуль'),
+(5, 'Правдивый стих о тестировщике', 'Поэзия', 'Олег Дудка'),
+(6, 'Человек-паук', 'Фантастика', 'Амовский Михаил'),
+(7, 'Гаррик Шпроттер', 'Дарк Фэнтези', 'Дарья Донцова');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `operation`
+--
+
+CREATE TABLE `operation` (
+  `operation_id` int NOT NULL,
+  `library_card` int NOT NULL,
+  `book_id` int NOT NULL,
+  `date_take` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_return` timestamp NULL DEFAULT NULL,
+  `status` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `library_card` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `surname`, `library_card`) VALUES
+(1, 'Кирилл', 'Рогов', 101),
+(2, 'Василиса', 'Иванова', 102),
+(3, 'Чарльз', 'Уоткинс', 103),
+(4, 'Василий', 'Рогов', 104);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `operation`
+--
+ALTER TABLE `operation`
+  ADD PRIMARY KEY (`operation_id`),
+  ADD UNIQUE KEY `you_have_this_book` (`library_card`,`book_id`,`status`) USING BTREE,
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `library_card` (`library_card`) USING BTREE;
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `library_card` (`library_card`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `operation`
+--
+ALTER TABLE `operation`
+  MODIFY `operation_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `operation`
+--
+ALTER TABLE `operation`
+  ADD CONSTRAINT `operation_ibfk_1` FOREIGN KEY (`library_card`) REFERENCES `user` (`library_card`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `operation_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
